1895d11dff69e5a19be3112e9dcbd589f5a52d7a
Vlad Kapitan <v.kapitan2010@gmail.com>
2024-03-05 17:08:47 +0300

    refactor: remove withMinMaxDate hoc from CalendarDays and similar to DayCell

--- src/components/DatePicker/Calendar/Body/CalendarDays/DayCell/index.tsx
+++ src/components/DatePicker/Calendar/Body/CalendarDays/DayCell/index.tsx
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 import { IDay } from '../../../../../../utils/createDay';
 import { ContextData } from '../../../../Context';
 import { withCurrentDate } from './hoc/withCurrentDate';
+import { withMinMaxDate } from './hoc/withMinMaxDate';
 import { Root } from './Root';
 
 interface IDayCellProps {
   day: IDay;
 }
 
-type IComponentHOC = typeof Root | ReturnType<typeof withCurrentDate>;
+type IComponentHOC =
+  | typeof Root
+  | ReturnType<typeof withCurrentDate>
+  | ReturnType<typeof withMinMaxDate>;
 
 class DayCell extends Component<IDayCellProps> {
   static contextType = ContextData;
@@ -17,9 +21,14 @@ class DayCell extends Component<IDayCellProps> {
 
   render() {
     const { day } = this.props;
+    const { minDate, maxDate } = this.context.config;
 
     let ComponentHOC: IComponentHOC = Root;
     ComponentHOC = withCurrentDate(ComponentHOC);
+
+    if (minDate || maxDate) {
+      ComponentHOC = withMinMaxDate(ComponentHOC);
+    }
 
     return <ComponentHOC day={day} />;
   }
