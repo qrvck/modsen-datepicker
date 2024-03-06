ae4db66eb33fe3df51887fa6935211069f9d340e
Vlad Kapitan <v.kapitan2010@gmail.com>
2024-03-06 23:21:22 +0300

    feat: do draft displaying selected date in input

--- src/components/DatePicker/Input/index.tsx
+++ src/components/DatePicker/Input/index.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
+import { ContextData } from '../Context';
+
 class Input extends Component {
+  static contextType = ContextData;
+  declare context: React.ContextType<typeof ContextData>;
+
+  getValue = () => {
+    const { selectedDay } = this.context.singleSelect;
+
+    if (selectedDay) {
+      const { dayNumber, monthNumber, year } = selectedDay;
+      return `${dayNumber}/${monthNumber}/${year}`;
+    } else {
+      return '';
+    }
+  };
+
   render() {
-    return <input type="number" name="" id="" />;
+    const { getValue } = this;
+    console.log(getValue());
+
+    return <input type="text" name="" id="" value={getValue()} />;
   }
 }
 
