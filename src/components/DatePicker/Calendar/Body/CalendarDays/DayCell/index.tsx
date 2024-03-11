1a6819c48fcf5e18a77f0c4a4d153186a5d7b8f2
Vlad Kapitan <v.kapitan2010@gmail.com>
2024-03-11 20:43:07 +0300

    feat: do correct turnover in hoc of day cell

--- src/components/DatePicker/Calendar/Body/CalendarDays/DayCell/index.tsx
+++ src/components/DatePicker/Calendar/Body/CalendarDays/DayCell/index.tsx
@@ -17,26 +17,42 @@ interface IDayCellProps {
   day: IDay;
 }
 
+interface IDayCellState {
+  minDate?: Date;
+  maxDate?: Date;
+  range: boolean;
+  weekends: boolean;
+  holidays: boolean;
+  todoList: boolean;
+}
+
 type IComponentHOC =
   | typeof Root
   | ReturnType<typeof withCurrentDate>
   | ReturnType<typeof withMinMaxDate>;
 
-class DayCell extends Component<IDayCellProps> {
+class DayCell extends Component<IDayCellProps, IDayCellState> {
   static contextType = ContextData;
   declare context: React.ContextType<typeof ContextData>;
 
-  render() {
-    const { day } = this.props;
+  state = {
+    minDate: this.context.config.minDate,
+    maxDate: this.context.config.maxDate,
+    range: this.context.config.range,
+    weekends: this.context.config.weekends,
+    holidays: this.context.config.holidays,
+    todoList: this.context.config.todoList,
+  };
+
+  ComponentHOC = this.configureComponent();
+
+  configureComponent() {
     const { minDate, maxDate, range, weekends, holidays, todoList } = this.context.config;
 
     let ComponentHOC: IComponentHOC = Root;
     ComponentHOC = withCurrentDate(ComponentHOC);
 
-    if (minDate || maxDate) {
-      ComponentHOC = withMinMaxDate(ComponentHOC);
-    }
-
+    if (minDate || maxDate) ComponentHOC = withMinMaxDate(ComponentHOC);
     if (weekends) ComponentHOC = withWeekends(ComponentHOC);
     if (holidays) ComponentHOC = withHolidays(ComponentHOC);
     if (todoList) ComponentHOC = withTodoList(ComponentHOC);
@@ -46,6 +62,38 @@ class DayCell extends Component<IDayCellProps> {
     } else {
       ComponentHOC = withRangeSelect(ComponentHOC);
     }
+
+    return ComponentHOC;
+  }
+
+  componentDidUpdate() {
+    const { state } = this;
+    const { minDate, maxDate, range, weekends, holidays, todoList } = this.context.config;
+
+    if (
+      minDate !== state.minDate ||
+      maxDate !== state.maxDate ||
+      range !== state.range ||
+      weekends !== state.weekends ||
+      holidays !== state.holidays ||
+      todoList !== state.todoList
+    ) {
+      this.ComponentHOC = this.configureComponent();
+
+      this.setState({
+        minDate,
+        maxDate,
+        range,
+        weekends,
+        holidays,
+        todoList,
+      });
+    }
+  }
+
+  render() {
+    const { day } = this.props;
+    const { ComponentHOC } = this;
 
     return <ComponentHOC day={day} />;
   }
