d24801b569a64ae5358621273a741a30b7c24460
Vlad Kapitan <v.kapitan2010@gmail.com>
2024-03-14 01:04:00 +0300

    feat: set english language to locale

--- src/utils/createDate.ts
+++ src/utils/createDate.ts
@@ -11,19 +11,19 @@ function getWeekNumberInYear(date: Date) {
 function createDate(date: Date, firstDayOfWeek: 'sunday' | 'monday' = 'monday') {
   const dayNumber = date.getDate(); // текущее число месяца (25 число)
   const fullDayNumber = dayNumber < 10 ? `0${dayNumber}` : `${dayNumber}`;
-  const dayName = date.toLocaleDateString('default', { weekday: 'long' }); // название дня недели (воскресенье)
-  const shortDayName = date.toLocaleDateString('default', { weekday: 'short' }); // название дня недели (вс)
+  const dayName = date.toLocaleDateString('en-US', { weekday: 'long' }); // название дня недели (воскресенье)
+  const shortDayName = date.toLocaleDateString('en-US', { weekday: 'short' }); // название дня недели (вс)
   let dayNumberInWeek = date.getDay() === 0 ? 7 : date.getDay(); // номер дня в неделе (понедельник = 1; воскресенье = 7 день)
   if (firstDayOfWeek === 'sunday') dayNumberInWeek = date.getDay() + 1; // если первый день sunday - воскресенье = 1; понедельник = 2
 
   const monthNumber = date.getMonth() + 1; // число месяца (2)
   const fullMonthNumber = monthNumber < 10 ? `0${monthNumber}` : `${monthNumber}`;
   const monthIndex = date.getMonth(); // индекс месяца
-  const monthName = date.toLocaleDateString('default', { month: 'long' }); // февраль
-  const shortMonthName = date.toLocaleDateString('default', { month: 'short' }); // февр.
+  const monthName = date.toLocaleDateString('en-US', { month: 'long' }); // февраль
+  const shortMonthName = date.toLocaleDateString('en-US', { month: 'short' }); // февр.
 
   const year = date.getFullYear(); // год 2024
-  const shortYear = date.toLocaleDateString('default', { year: '2-digit' }); // год 24
+  const shortYear = date.toLocaleDateString('en-US', { year: '2-digit' }); // год 24
 
   const timestamp = date.getTime(); // 234324324324134134
   const weekNumberInYear = getWeekNumberInYear(date); // номер недели в году (8)
